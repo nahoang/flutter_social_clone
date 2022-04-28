8d7a8eccc1578234a872c1359e7ef3cb7f023622
Henry Nguyen <nahoang9698@gmail.com>
2022-04-28 14:04:09 +0700

    feat: Add firebase core aand configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_social_clone/pages/home.dart';
 
-void main() {
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/pages/timeline.dart
+++ lib/pages/timeline.dart
@@ -1,19 +1,48 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_social_clone/widgets/progress.dart';
 
 import '../widgets/header.dart';
 
+final usersRef = Firestore.instance.collection('users');
+
 class Timeline extends StatefulWidget {
+
   @override
   _TimelineState createState() => _TimelineState();
 }
 
 class _TimelineState extends State<Timeline> {
+  List<dynamic> users = [];
+
+  @override
+  void initState() {
+    getUsers();
+    super.initState();
+  }
+
+  getUsers() async {
+    final QuerySnapshot snapshot = await usersRef.getDocuments();
+
+    setState(() {
+      users = snapshot.documents;
+      print('users: $users');
+    });
+
+    // userRef.getDocuments().then((QuerySnapshot snapshot) {
+    //   snapshot.documents.forEach((DocumentSnapshot doc) {
+    //     print(doc.data);
+    //   });
+    // });
+  }
+
   @override
   Widget build(context) {
     return Scaffold(
       appBar: header(context, isAppTitle: true),
-      body: Text('profile')
+      body: Container(
+        child: ListView(children: users.map((user) => Text(user['username'])).toList(),)
+      )
     );
   }
 }
