c383cd3cc3248c25eb39e49ea5398307c46f3e00
Henry Nguyen <nahoang9698@gmail.com>
2022-05-06 18:35:53 +0700

    feat: Implement Edit profile UI

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_social_clone/pages/edit_profile.dart';
 import 'package:flutter_social_clone/pages/home.dart';
 import 'package:flutter_social_clone/widgets/header.dart';
 import 'package:flutter_social_clone/widgets/progress.dart';
@@ -10,39 +11,75 @@ import '../models/user.dart';
 class Profile extends StatefulWidget {
   final String? profileId;
 
-  Profile({ this.profileId });
+  Profile({this.profileId});
 
   @override
   _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+  final String? currentUserId = currentUser?.id;
 
   Column buildCountColumn(String label, int count) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text(count.toString(),
-        style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+        Text(
+          count.toString(),
+          style: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
         ),
         Container(
           margin: EdgeInsets.only(top: 4.0),
-          child: Text(
-            label,
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 15.0,
-              fontWeight: FontWeight.bold
-            )
-          ),
+          child: Text(label,
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold)),
         ),
       ],
     );
   }
 
+  Container buildButton({required String text, VoidCallback? function}) {
+    return Container(
+      padding: EdgeInsets.only(top: 2.0),
+      child: FlatButton(
+        onPressed: function,
+        child: Container(
+          width: 240.0,
+          height: 27.0,
+          child: Text(text,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            border: Border.all(
+              color: Colors.blue,
+            ),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+      ),
+    );
+  }
+
+  editProfile() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => EditProfile(
+      currentUserId: currentUserId
+    )));
+  }
+
   buildProfileButton() {
-    return Text('profile button');
+    //viewing your own profile.
+    bool isProfileOwner = currentUserId == widget.profileId;
+    if (isProfileOwner) {
+      return buildButton(text: 'Edit Profile', function: editProfile);
+    }
   }
 
   buildProfileHeader() {
@@ -63,7 +100,8 @@ class _ProfileState extends State<Profile> {
                     CircleAvatar(
                       radius: 40.0,
                       backgroundColor: Colors.grey,
-                      backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+                      backgroundImage:
+                          CachedNetworkImageProvider(user.photoUrl),
                     ),
                     Expanded(
                       flex: 1,
@@ -80,9 +118,7 @@ class _ProfileState extends State<Profile> {
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              buildProfileButton()
-                            ],
+                            children: [buildProfileButton()],
                           ),
                         ],
                       ),
@@ -120,19 +156,15 @@ class _ProfileState extends State<Profile> {
               ],
             ),
           );
-        }
-    );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: header(context, titleText: 'Profile'),
-      body: ListView(
-        children: [
-          buildProfileHeader()
-        ],
-      )
-    );
+        appBar: header(context, titleText: 'Profile'),
+        body: ListView(
+          children: [buildProfileHeader()],
+        ));
   }
 }
