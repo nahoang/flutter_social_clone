06a1dcc68a15ad942fea9238a36d5fd2b85d6a84
Henry Nguyen <nahoang9698@gmail.com>
2022-05-10 15:03:02 +0700

    feat: Update profile to firestore

--- lib/pages/edit_profile.dart
+++ lib/pages/edit_profile.dart
@@ -9,18 +9,20 @@ import '../models/user.dart';
 class EditProfile extends StatefulWidget {
   final String? currentUserId;
 
-  EditProfile({ this.currentUserId });
-
+  EditProfile({this.currentUserId});
 
   @override
   _EditProfileState createState() => _EditProfileState();
 }
 
 class _EditProfileState extends State<EditProfile> {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
   TextEditingController displayNameController = TextEditingController();
   TextEditingController bioController = TextEditingController();
   bool isLoading = false;
   late User user;
+  bool _displayNameValid = true;
+  bool _bioValid = true;
 
   @override
   void initState() {
@@ -45,14 +47,15 @@ class _EditProfileState extends State<EditProfile> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(padding: EdgeInsets.only(top: 12.0),
-          child: Text('Display Name',
-          style: TextStyle(color: Colors.grey)),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+          child: Text('Display Name', style: TextStyle(color: Colors.grey)),
         ),
         TextField(
           controller: displayNameController,
           decoration: InputDecoration(
             hintText: 'Update display name',
+            errorText: _displayNameValid ? null : 'Display Name too short',
           ),
         )
       ],
@@ -63,86 +66,114 @@ class _EditProfileState extends State<EditProfile> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(padding: EdgeInsets.only(top: 12.0),
-          child: Text('Bio',
-              style: TextStyle(color: Colors.grey)),
+        Padding(
+          padding: EdgeInsets.only(top: 12.0),
+          child: Text('Bio', style: TextStyle(color: Colors.grey)),
         ),
         TextField(
           controller: bioController,
           decoration: InputDecoration(
             hintText: 'Update bio',
+            errorText: _bioValid ? null : 'Bio is too long',
           ),
         )
       ],
     );
   }
 
+  updateProfileData() {
+    setState(() {
+      displayNameController.text.trim().length < 3 ||
+              displayNameController.text.isEmpty
+          ? _displayNameValid = false
+          : _displayNameValid = true;
+      bioController.text.trim().length > 100 ? _bioValid = false : _bioValid = true;
+    });
+
+    if (_displayNameValid && _bioValid) {
+      usersRef.doc(widget.currentUserId).update({
+        'displayName': displayNameController.text,
+        'bio': bioController.text,
+      });
+      SnackBar snackbar = SnackBar(content: Text('Profile updated!'));
+      _scaffoldKey.currentState?.showSnackBar(snackbar);
+    }
+  }
+
+  logout() async {
+    await googleSignIn.signOut();
+    Navigator.push(context, MaterialPageRoute(builder: (context) => Home()));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         backgroundColor: Colors.white,
-        title: Text(
-          'Edit profile',
-          style: TextStyle(
-            color: Colors.black,
-          )
-        ),
+        title: Text('Edit profile',
+            style: TextStyle(
+              color: Colors.black,
+            )),
         actions: [
-          IconButton(onPressed: () => Navigator.pop(context),
-            icon: Icon(Icons.done, size: 30.0, color: Colors.green),)
+          IconButton(
+            onPressed: () => Navigator.pop(context),
+            icon: Icon(Icons.done, size: 30.0, color: Colors.green),
+          )
         ],
       ),
-      body: isLoading ?
-              circularProgress() : ListView(
-        children: [
-          Container(
-            child: Column(
+      body: isLoading
+          ? circularProgress()
+          : ListView(
               children: [
-                Padding(
-                  padding: EdgeInsets.only(top: 16.0, bottom: 8.0,),
-                  child: CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: CachedNetworkImageProvider(user.photoUrl),
-                ),
-                ),
-                Padding(padding: EdgeInsets.all(16.0),
+                Container(
                   child: Column(
                     children: [
-                      buildDisplayNameField(),
-                      buildBioField(),
-                    ]
-                  )
-                ),
-                RaisedButton(onPressed: ()  => print('update profile data'),
-                  child: Text(
-                    'Update profile',
-                    style: TextStyle(
-                      color: Theme.of(context).primaryColor,
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    )
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(16.0),
-                  child: FlatButton.icon(
-                    onPressed: () => print('logout'),
-                    icon: Icon(Icons.cancel, color: Colors.red),
-                    label: Text(
-                      'Logout',
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontSize: 20.0,
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 16.0,
+                          bottom: 8.0,
+                        ),
+                        child: CircleAvatar(
+                          radius: 50.0,
+                          backgroundImage:
+                              CachedNetworkImageProvider(user.photoUrl),
+                        ),
                       ),
-                    ),
+                      Padding(
+                          padding: EdgeInsets.all(16.0),
+                          child: Column(children: [
+                            buildDisplayNameField(),
+                            buildBioField(),
+                          ])),
+                      RaisedButton(
+                        onPressed: updateProfileData,
+                        child: Text('Update profile',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            )),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(16.0),
+                        child: FlatButton.icon(
+                          onPressed: logout,
+                          icon: Icon(Icons.cancel, color: Colors.red),
+                          label: Text(
+                            'Logout',
+                            style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 20.0,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
                 )
               ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
